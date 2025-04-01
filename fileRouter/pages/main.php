172209c8744a433b9aeb8b157e5e71d00e936571
SwordsRuby <129753520+SwordsRuby@users.noreply.github.com>
2025-04-01 21:16:57 +0300

    Delete fileRouter directory

--- fileRouter/pages/main.php
+++
@@ -1,3 +0,0 @@
-<a href="page_1">page_1</a>
-<a href="page_2">page_2</a>
-<a href="img">img</a>
--- fileRouter/pages/page_1.php
+++
@@ -1,8 +0,0 @@
-<form method="post">
-    <input type="text" name="text">
-</form>
-<? if (isset($_POST['text'])) {
-    setcookie('text', password_hash($_POST['text'], PASSWORD_DEFAULT), time() + 3600);
-} ?>
-<p><?= isset($_COOKIE['text']) ? $_COOKIE['text'] : ''; ?></p>
-<a href="main">main</a>
