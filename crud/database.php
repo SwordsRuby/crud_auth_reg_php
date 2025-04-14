e4d741ca0c4c0b61562784b74d3eae0b6a63f932
SwordsRuby <129753520+SwordsRuby@users.noreply.github.com>
2025-04-14 11:54:02 +0300

    Delete crud directory

--- crud/database.php
+++
@@ -1,6 +0,0 @@
-<?
-try {
-    $sql = new PDO('mysql:host=MySQL-8.2;dbname=myBlog;charset=utf8;', 'root', '');
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
--- crud/main.js
+++
@@ -1,4 +0,0 @@
-function deleteConfirm() {
-    const conf_block = document.getElementsByClassName('confirm_block');
-    conf_block[0].classList.toggle('show');
-}
