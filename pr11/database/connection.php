dbcf1d55e4a36f7b1a23efef0b0e752db7616f3f
SwordsRuby <129753520+SwordsRuby@users.noreply.github.com>
2025-04-14 11:50:15 +0300

    Delete pr11 directory

--- pr11/database/connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-try{
-    $database= new PDO("mysql:host=MySQL-8.2;dbname=store;charset=utf8", "root", "");
-}catch(PDOException $error){
-    die("Ошибка подключения к БД" . $error->getMessage());
-}
-?>
